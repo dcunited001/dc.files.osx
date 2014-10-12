f303fead4478c7632b70736b2154653f89b32905
David Conner <dconner.pro@gmail.com>
2014-10-12 00:36:16 -0400

    added brew cask apps to a setup script

---
+++ apps.sh
@@ -0,0 +1,11 @@
+# install cask
+brew install caskroom/cask/brew-cask
+
+# install apps:
+brew cask install soundcleod ccmenu seil karabiner android-studio android-file-transfer
+brew cask install angry-ip-scanner quicksilver calibre controllermate dash divvy flowdock
+brew cask install harvest intellij-idea-ce iterm2 keka lighttable sublime-text limechat
+brew cask install electrum nitrous-desktop rubymine skype spotify stay torbrowser utorrent
+brew cask install virtualbox vagrant vlc webstorm vagrant
+
+# missing: pixelmater sourcetree
